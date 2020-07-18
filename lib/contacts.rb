3ce2c1e81d34710a5c6b9df91dfc44a76f2f3145
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 17:56:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -30,6 +30,7 @@ def remove_strawberry(contacts)
 
   contacts.each do |person, contact_details_hash|
   end
+  contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
